98da725c12addcadb7de5c261ba3eee682a7061b
Ryan Lin-pc <ryanlin0228@gmail.com>
2023-04-18 02:35:07 -0400

    lab08

--- lab8/RyanLinLAB08HWQ1.c
+++ lab8/RyanLinLAB08HWQ1.c
@@ -1,28 +1,50 @@
 
-#include <stdio.h>
-#include <string.h>
 #include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-// function ToPigLatin
-char *ToPigLatin(char *word)
-{
-  if (strlen(word) == 0 || !isalpha(word[0]))
-  {
-    return word;
+int is_Vowel(char c) {
+
+  if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' ||
+      c == 'E' || c == 'I' || c == 'O' || c == 'U') {
+    return 1;
+  } else {
+    return 0;
   }
-  bool is_
+}
+// function ToPigLatin
+char *ToPigLatin(char *word) {
+  // check if the entered word is a word
+  int i;
+  int length = 1;
+  char *result = (char *)malloc(sizeof(char) * 100);
+  // check if first letter is uppercase or not
+  int capital = isupper(word[0]);
+  while (word[i] != '\0') {
+
+    if (tolower(word[0]) == 'a' || tolower(word[0]) == 'e' ||
+        tolower(word[0]) == 'i' || tolower(word[0]) == 'o' ||
+        tolower(word[0]) == 'u') {
+      strcpy(result, word);
+      strcpy(result, "way");
+    }
+    // else if (is_Vowel(word[0]) != 1 || word[0] != 0)
+    // {
+
+    // }
+
+    return result;
+  }
 }
 
-// driver function
-int main()
-{
+// main
+int main() {
 
   char word[39];
-  char result;
   printf("Enter a word: ");
-  scanf("%c", word);
-  result = ToPigLatin(word);
+  scanf("%s", word);
 
   // displaying result
-  printf("The Pig Latin of %c is: %c", word, result);
+  printf("Output : %s", ToPigLatin(word));
 }
