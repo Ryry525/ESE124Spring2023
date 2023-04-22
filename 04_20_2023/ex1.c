7503daeca4c6f0daffd63e6afdfee5fc481c9c95
Ryan Lin <Ryanlin0228@gmail.com>
2023-04-22 19:40:01 -0400

    04_20_2023 Lecture

---
+++ 04_20_2023/ex1.c
@@ -0,0 +1,42 @@
+#define STRLEN 30
+#include <stdio.h>
+#include <string.h>
+#define SHLEN 3
+#define NR_ADDRESSES 10
+
+typedef struct
+{
+    int number;
+    char street [STRLEN];
+    char town [STRLEN];
+    int zip;
+    char state [SHLEN];
+} AddressType;
+
+int main(){
+AddressType B[NR_ADDRESSES];
+
+int i, Nra = 3;
+
+for(i = 0; i < Nra; i++){
+printf("Number: ");
+scanf("%d", &B[i].number);
+
+printf("Street: ");
+scanf("%s", &B[i].street);
+getchar();
+
+printf("Town: ");
+scanf("%s", &B[i].town);
+
+
+printf("Zip: ");
+scanf("%d", &B[i].zip);
+
+printf("State: ");
+scanf("%s", &B[i].state);
+getchar();
+}
+for(i=0; i < Nra; i++)
+printf("Address: %d %s %s %d %s\n", B[i].number,B[i].street, B[i].town, B[i].zip, B[i].state);
+}
